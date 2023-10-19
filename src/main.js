dbadc08dd01d85f52bd9c87bf03234697212d334
George-Gabechava <108359813+George-Gabechava@users.noreply.github.com>
2023-10-19 14:56:00 -0400

    no UI, no localStorage, assignTask working

--- src/main.js
+++ src/main.js
@@ -1,54 +1,47 @@
 import { format, compareAsc } from 'date-fns';
 import { isStorageAvailable } from './checkStorage';
 
-// console.log('main.js imported successfully.');
-
-
 //check if localStorage is available
 isStorageAvailable();
 
 //Lit of Project Groups
-// let myProjects = [];
-
 ///////////////// THE WINDOW'S OPEN
 window.myProjects = [];
-
-
-
 
 
 ////Functions for the To Do List
 ////Each project / To Do item should be editable, deletable, & completable.
 
-//Create a project group
+//Create a project group to store tasks
 function createProject(name) {
     const projectName = name;
-    const tasklist = {};
+    const taskList = [];
 
-    return {projectName, tasklist};
+    return {projectName, taskList};
 }
 
 //Currently selected project group
-//Show Projects button --> select 
+//Show Projects button UI should allow user to select 
 function currentProject() {
     //localStorage not implemented yet, set current project to 'default'
     //if myProjects in localStorage is empty:
 
-
-    return default1.projectName;
+    return Personal.projectName;
 
     //else, load myProjects from localStorage
-    //some code
+    //'some code'
 }
 
-const default1 = createProject('default1');
-myProjects.push(default1);
+///////// Dummy Projects for Testing
+const Personal = createProject('Personal');
+myProjects.push(Personal);
 
-const default2 = createProject('default2');
-myProjects.push(default2);
+const Work = createProject('Work');
+myProjects.push(Work);
 
+//Set Current Project on Home Page to Personal for testing
 currentProject();
-console.log('currentP', currentProject());
+
 
 //Create a To Do Task
 function createTask(title, description, dueDate, priority) {
@@ -62,34 +55,25 @@ function createTask(title, description, dueDate, priority) {
 
 //Asign the To Do task to the current project
 function assignTask(task) {
+    //Find current open project
     const findProjectIndex = myProjects.findIndex(object => object.projectName === `${currentProject()}`);
-    console.log('index', findProjectIndex);
 
-    //task names need to be unique, maybe make the task name the title.
-
-
-    //if someone enters the same task title, find # of similar, then add that number to the end of the new task
-    myProjects[findProjectIndex].tasklist = Object.assign(myProjects[findProjectIndex].tasklist, {task});
+    //Add task to current project
+    myProjects[findProjectIndex].taskList.push(task);
+    
+    console.log("current taskList", myProjects[findProjectIndex].taskList);
+    
 }
 
-
-///////////////// THE WINDOW'S OPEN
+//Dummy Tasks for testing
 const task1 = createTask('Code this assignment', 'This TOP assignment', 3, 10);
 assignTask(task1);
-console.log('current tasklist1', myProjects[0].tasklist);
-const task2 = createTask('more coding every day', '', 4, 9);
+const task2 = createTask('grocery shopping', '', 4, 9);
 assignTask(task2);
-console.log('current tasklist2', myProjects[0].tasklist);
 
 window.task = createTask('gym every day', '', 1, 7);
 window.task4 = createTask('sleep 8 hours', 'zzz', 6, 6);
 
+console.log("myProjects", myProjects);
 
-
-// console.log('task1', task1);
-
-//Object.assign(myProjects[0].tasklist, {task1})
-
-
-console.log(myProjects[0]);
 
