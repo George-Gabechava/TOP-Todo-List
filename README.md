8a7afb41360af9624fba56b0149ff5a19c242645
George-Gabechava <108359813+George-Gabechava@users.noreply.github.com>
2025-09-24 10:43:52 -0400

    Added site link in README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # TOP-Todo-List
-Todo List
+
+Todo List which uses localStorage JSON to keep track of To Do tasks.
+
+Site: https://george-gabechava.github.io/TOP-Todo-List/
--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import './style.css';
-import './main.js';
+import "./style.css";
+import "./main.js";
 
-import { createTask } from './main.js';
-
-// console.log("Hello Worlds!");
+import { createTask } from "./main.js";
